e7522a0854f435cb0ec5a509fa0ca50f2f3b425d
Apoorva <asomanah@student.42berlin.de>
2024-07-11 18:49:26 +0200

    strcpy util

---
+++ utils/ft_strcpy.c
@@ -0,0 +1,31 @@
+#include "../so_long_bonus.h"
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	size_t	destlen;
+	int		i;
+
+	i = 0;
+	destlen = ft_strlen(dest);
+	while (src[i])
+	{
+		dest[destlen + i] = src[i];
+		i++;
+	}
+	dest[destlen + i] = '\0';
+	return (dest);
+}
